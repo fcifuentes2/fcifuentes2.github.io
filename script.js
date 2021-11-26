1cdd0b7672c875d8272bb05006b118f8bd5859f7
fcifuentes2 <75434356+fcifuentes2@users.noreply.github.com>
2021-11-25 23:36:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+
+// carga la funcion despues de que se cargue por completo el documento
+window.onload = function(){
+    addEventListener('change', obtenerTotal);
+}
+// buscar el <form> y guardarlo en una variable
+const form = document.querySelector('form');
+
+// decir que en el evento enviar se sume el total
+form.addEventListener('change', obtenerTotal);
+
+
+function obtenerTotal(event) {
+
+// evitar enviar el formulario que ocasiona que se refresque la pagina
+event.preventDefault();
+
+// obtener todos los input radio y checkbox
+const planRadio = document.querySelector('input[name=plan]:checked')
+const rvoRadio = document.querySelector('input[name=rvo]:checked')
+const equiposRadio = document.querySelector('input[name=equipos]:checked')
+const premium1Check = document.querySelector('input[name=premium1]:checked')
+const premium2Check = document.querySelector('input[name=premium2]:checked')
+const premium3Check = document.querySelector('input[name=premium3]:checked')
+const premium4Check = document.querySelector('input[name=premium4]:checked')
+const premium5Check = document.querySelector('input[name=premium5]:checked')
+const premium6Check = document.querySelector('input[name=premium6]:checked')
+const premium7Check = document.querySelector('input[name=premium7]:checked')
+const premium8Check = document.querySelector('input[name=premium8]:checked')
+const premium9Check = document.querySelector('input[name=premium9]:checked')
+const premium10Check = document.querySelector('input[name=premium10]:checked')
+const premium11Check = document.querySelector('input[name=premium11]:checked')
+const premium12Check = document.querySelector('input[name=premium12]:checked')
+const rvp1Check = document.querySelector('input[name=rvp1]:checked')
+const rvp2Check = document.querySelector('input[name=rvp2]:checked')
+const rvp3Check = document.querySelector('input[name=rvp3]:checked')
+const rvp4Check = document.querySelector('input[name=rvp4]:checked')
+const rvp5Check = document.querySelector('input[name=rvp5]:checked')
+const rvp6Check = document.querySelector('input[name=rvp6]:checked')
+const rvp7Check = document.querySelector('input[name=rvp7]:checked')
+const rvp8Check = document.querySelector('input[name=rvp8]:checked')
+const rvoCheck = document.querySelector('input[name=rvoEquipo]:checked')          
+
+// convertir los valores a String, o colocar un 0 si no elegieron algo
+const plan = planRadio ?  Number(planRadio.value): 0
+const rvo = rvoRadio ?  Number(rvoRadio.value): 0
+const equipos = equiposRadio ?  Number(equiposRadio.value): 0
+const premium1 = premium1Check ?  Number(premium1Check.value): 0
+const premium2 = premium2Check ?  Number(premium2Check.value): 0
+const premium3 = premium3Check ?  Number(premium3Check.value): 0
+const premium4 = premium4Check ?  Number(premium4Check.value): 0
+const premium5 = premium5Check ?  Number(premium5Check.value): 0
+const premium6 = premium6Check ?  Number(premium6Check.value): 0
+const premium7 = premium7Check ?  Number(premium7Check.value): 0
+const premium8 = premium8Check ?  Number(premium8Check.value): 0
+const premium9 = premium9Check ?  Number(premium9Check.value): 0
+const premium10 = premium10Check ?  Number(premium10Check.value): 0
+const premium11 = premium11Check ?  Number(premium11Check.value): 0
+const premium12 = premium12Check ?  Number(premium12Check.value): 0
+const rvp1 = rvp1Check ?  Number(rvp1Check.value): 0
+const rvp2 = rvp2Check ?  Number(rvp2Check.value): 0
+const rvp3 = rvp3Check ?  Number(rvp3Check.value): 0
+const rvp4 = rvp4Check ?  Number(rvp4Check.value): 0
+const rvp5 = rvp5Check ?  Number(rvp5Check.value): 0
+const rvp6 = rvp6Check ?  Number(rvp6Check.value): 0
+const rvp7 = rvp7Check ?  Number(rvp7Check.value): 0
+const rvp8 = rvp8Check ?  Number(rvp8Check.value): 0
+const rvoEquipo = rvoCheck ?  Number(rvoCheck.value): 0
+
+//suma total se le asigna a variable result
+const result = plan + rvo + equipos + premium1 + premium2 + premium3 + premium4 + 
+premium5 + premium6 + premium7 + premium8 + premium9 + premium10 + premium11 +
+premium12 + rvp1 + rvp2 + rvp3 + rvp4 + rvp5 + rvp6 + rvp7 + rvp8 + rvoEquipo;
+
+//se asigna valor de variable result a resultado para poder ser mostrado en pantalla
+document.getElementById('resultado').innerHTML = result;
+
+}
